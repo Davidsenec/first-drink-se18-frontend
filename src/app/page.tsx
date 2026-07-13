86aa5886a3edebd41c263c8b50fe0b0aaf80c031
Tharathep Laohaviroj <tuner.tharathep@gmail.com>
2026-07-13 07:59:16 +0700

    chore: refractor code and add comments to separate elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
   const inputBg = "bg-[#1c1f2e]";
   return (
     <div className="flex flex-col min-h-screen items-center justify-center font-mono">
+
+      {/* Header */}
       <div className="flex flex-col text-center">
         <div className="text-3xl">⚽</div>
         <div className="text-[#5b6c82] text-[14px]">
@@ -15,25 +17,40 @@ export default function Home() {
           <strong>Software Engineering</strong>
         </div>
       </div>
-      <div className="mt-8 mb-4 text-[40px]">
-        <strong>Login</strong>
-      </div>
+
+      {/* Login Text */}
+      <div className="mt-8 mb-4 text-[40px]"> <strong>Login</strong> </div>
+
+      {/* Username Input */}
       <input
-      type="text"
-      placeholder="Username"
-      className={`${inputBg} ${box_sizing} ${inputField}`}
+        type="text"
+        placeholder="Username"
+        className={`${inputBg} ${box_sizing} ${inputField}`}
       />
+
+      {/* Password Input */}
       <input
-      type="password"
-      placeholder="Password"
-      className={`${inputBg} ${box_sizing} ${inputField}`}
+        type="password"
+        placeholder="Password"
+        className={`${inputBg} ${box_sizing} ${inputField}`}
       />
-      <button className={`bg-[#f0990d] hover:bg-[#de8d0b] text-[#0b0b0f] font-bold ${box_sizing}`}>
+
+      {/* Login Button */}
+      <button className=
+        {`bg-[#f0990d]
+          hover:bg-[#de8d0b]
+          text-[#0b0b0f]
+          font-bold
+          ${box_sizing}`}
+      >
         Login
       </button>
+
+      {/* Register page link text */}
       <div className={`${grayText} font-sans mt-2`}>
         New here? <a href="register" className={`${yellowText}`}>Sign up here</a>
       </div>
+
     </div>
   );
 }
