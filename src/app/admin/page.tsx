658da3daa19469e5caa4163d592c7e9a7dba8070
Davidsenec <sgd88.thomponsenec@yahoo.com>
2026-07-18 15:38:47 +0700

    finish setup of info page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function AdminPage() {
     return (
-        <div></div>
+        <div>
+            <main className="flex justify-center px-3 py-6">
+                <div className="flex flex-col w-full max-w-xl px-4 py-8 gap-3">
+                    <Link href="../"><button className="mb-3 text-gray-500 hover:text-white ">&#8592; Back</button></Link>
+                </div>
+            </main>
+        </div>
     );
 }
--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -6,22 +6,34 @@ export default function InfoPage() {
         <main className="flex justify-center px-3 py-6">
             <div className="flex flex-col w-full max-w-xl px-4 py-8 gap-3">
                 <Link href="../"><button className="mb-3 text-gray-500 hover:text-white ">&#8592; Back</button></Link>
-                <h1 className="mb-6 text-5xl font-bold text-center">Welcome, user</h1>
+                <h1 className="mb-6 text-5xl font-bold">Welcome, user</h1>
                 <div className="bg-amber-100 rounded-2xl text-black text-center px-20 py-20">Image</div>
+                <label className="text-center text-gray-500 text-sm">SCREENSHOT TO SAVE</label>
 
                 <div className="border-[#dcdce9] rounded-xl bg-[#1B1D2F] p-4">
                     <h1 className="text-xs font-bold text-emerald-600">EVENT INFO</h1>
                     <h2>⏰ Monday, August 3 2026 · 19:00</h2>
                     <h3 className="">📍 Me Smile Cafe | Google maps: 
                         <Link href="https://maps.app.goo.gl/JAwqHpXVpaZadCN19" className="text-blue-500"> Click here!</Link></h3>
-                    <p>---</p>
+                    <br></br>
+                    <hr className="text-gray-500 p-3"></hr>
                     <h1 className="text-xs font-bold text-emerald-600">CONTACT INFO</h1>
-                    <h2>Discord: </h2>
+                    <h2>Discord: <Link href="{https://discord.gg/N79Fp3hZZ}" className="text-blue-500 text-sm">https://discord.gg/N79Fp3hZZ</Link></h2>
+                    <h2>Instagram: </h2>
                     <h2>Line: </h2>
                 </div>
 
                 <div className="border-[#dcdce9] rounded-xl bg-[#1B1D2F] p-4">
                     <h1 className="text-2xs font-bold text-emerald-600">RULES</h1>
+                    <ol className="list-decimal list-inside space-y-1 text-sm">
+                        <li>When arriving find a senior to scan your QR code for attendance</li>
+                        <li>Before going anywhere, inform a senior</li>
+                        <li>In an event of an emergency, find a senior for help</li>
+                        <li>Call your seniors by the correct name or else</li>
+                        <li>Be respectful to everyone</li>
+                        <li>Don't make a mess</li>
+                        <li>Don't drink too much ;)</li>
+                    </ol>
                 </div>               
                 
                 <Link href="edit-info"><Button> Edit Info</Button></Link>
