6cd05313814edd720c8df0f5d34788b4c6cb104b
Davidsenec <sgd88.thomponsenec@yahoo.com>
2026-07-21 00:50:49 +0700

    First iteration of the table

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -13,7 +13,33 @@ export default function AdminPage() {
                     <p>Departed: <label className="text-violet-500">0</label></p>
 
 
-                    
+                    <br></br> {/*temporary solution*/}
+                    <table className="w-full rounded-xl bg-[#1B1D2F]">
+                            <thead className="border-b border-gray-500">
+                            <tr className="">
+                                <th className="p-2">#</th>
+                                <th className="p-2">Username</th>
+                                <th className="p-2">Name</th>
+                                <th className="p-2">Status</th>
+                            </tr>
+                            </thead>
+
+                            <tbody className="">
+                            <tr>
+                                <td className="p-2">1</td>
+                                <td className="p-2">Ceo of sex</td>
+                                <td className="p-2">David</td>
+                                <td className="p-2">Not arrived</td>
+                            </tr>
+
+                            <tr>
+                                <td className="p-2">2</td>
+                                <td className="p-2">Gay lord</td>
+                                <td className="p-2">Auto</td>
+                                <td className="p-2">Present</td>
+                            </tr>
+                            </tbody>
+                    </table>
                 </div>
             </main>
         </div>
